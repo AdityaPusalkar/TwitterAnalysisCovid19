9eb7807c2acc46504053b0bb848e7c9f387f5efd
AdityaPusalkar <aditya.pusalkar@iitgn.ac.in>
2021-05-11 22:29:33 +0530

    Final Commit

--- twitter_extract.py
+++ twitter_extract.py
@@ -10,8 +10,8 @@ csvFile = open('./mydata/test.csv', 'a')
 # Use csv Writer
 csvWriter = csv.writer(csvFile)
 
-for tweet in tweepy.Cursor(api.search,q="#Covidiots",count=100,
+for tweet in tweepy.Cursor(api.search,q="#Pfizer",count=100,
                            lang="en",
-                           since="2021-04-07",tweet_mode='extended').items():
+                           since="2021-05-11",tweet_mode='extended').items():
     csvWriter.writerow([tweet.full_text.encode('utf-8'),tweet.retweet_count,tweet.user.followers_count,tweet.favorite_count,tweet.place,tweet.coordinates,tweet.geo,tweet.created_at])
 print('Done!')
